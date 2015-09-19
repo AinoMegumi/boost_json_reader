bc5add0d4791cb43053d91adc1abbfeb79ef54b7
yumetodo <yume-wikijp@live.jp>
2015-09-19 22:37:44 +0900

    さらに実験(一応成功)

--- ConsoleApplication5/Source.cpp
+++ ConsoleApplication5/Source.cpp
@@ -2,17 +2,22 @@
 #include <string>
 #include <boost/property_tree/ptree.hpp>
 #include <boost/property_tree/json_parser.hpp>
-#include <boost/foreach.hpp>
 #include <boost/optional.hpp>
 
-std::string data(const char* filepath, const char* directory) {
-	boost::property_tree::ptree pt;
-	read_json(filepath, pt);
-	auto value = pt.get_optional<std::string>(directory);
-	return value.get();
+std::string data(boost::property_tree::ptree& pt, const char* directory) {
+	return pt.get_optional<std::string>(directory).get();
 }
 
 int main(void) {
-	std::cout << data("test.json", "Data.str") << std::endl;
+	boost::property_tree::ptree pt;
+	read_json("test.json", pt);
+	std::cout << data(pt, "Data.str") << std::endl << std::endl;
+
+	for (auto& child : pt.get_child("Data.info")) {
+		std::cout
+			<< "id: " << child.second.get_optional<int>("id").get() << std::endl
+			<< "name:" << child.second.get_optional<std::string>("name").get() << std::endl;
+
+	}
 	return 0;
 }
