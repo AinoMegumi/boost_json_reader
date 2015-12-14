95ebd1c2fd19966c99eebbf6ec5d71b1515fa16c
AinoMegumi <aimegu2014@gmail.com>
2015-12-14 21:25:05 +0900

    std::stringに変換したら読めた。なんで？

--- ConsoleApplication5/Source.cpp
+++ ConsoleApplication5/Source.cpp
@@ -1,3 +1,4 @@
+#include <Windows.h>
 #include <iostream>
 #include <string>
 #include <boost/property_tree/ptree.hpp>
@@ -7,17 +8,26 @@
 #include <stdexcept>
 #include <type_traits>
 
+std::string WStringToString(std::wstring oWString) {
+	int iBufferSize = WideCharToMultiByte(CP_OEMCP, 0, oWString.c_str(), -1, (char *)NULL, 0, NULL, NULL);
+	char* cpMultiByte = new char[iBufferSize];
+	WideCharToMultiByte(CP_OEMCP, 0, oWString.c_str(), -1, cpMultiByte, iBufferSize, NULL, NULL);
+	std::string oRet(cpMultiByte, cpMultiByte + iBufferSize - 1);
+	delete[] cpMultiByte;
+	return oRet;
+}
+
 std::string data(boost::property_tree::ptree& pt, const char* directory) {
 	return pt.get_optional<std::string>(directory).get();
 }
 
-static std::wstring get_text_from_json(boost::property_tree::ptree& p, std::string tag) {
+static std::string get_text_from_json(boost::property_tree::ptree& p, std::string tag) {
 	auto target = p.get_optional<std::string>(tag);
 	if (!target) throw std::runtime_error("json reading failed");
 	static_assert(sizeof(wchar_t) == 2, "In function 'read json data' : Please check usage of 'std::codecvt_utf8_utf16'");
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
 	auto str = convert.from_bytes(target.get());
-	return str;
+	return WStringToString(str);
 }
 
 static int get_int_from_json(boost::property_tree::ptree& p, std::string tag) {
@@ -33,7 +43,7 @@ int main(void) {
 		auto& p = pt.get_child("Data");
 		std::cout << data(p, "str") << std::endl << std::endl;
 		for (auto& child : p.get_child("info")) {
-			std::wcout
+			std::cout
 				<< "id: " << get_int_from_json(child.second, "id") << std::endl
 				<< "name:" << get_text_from_json(child.second, "name") << std::endl << std::endl;
 		}
