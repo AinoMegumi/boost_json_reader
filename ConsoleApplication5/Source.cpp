9b45b21672d6b909b42bb7ae3b1f004b3f18bb9b
AinoMegumi <aimegu2014@gmail.com>
2016-01-24 19:46:26 +0900

    std::stringへの変換部分の削除およびstd::wcout.imbueを追加。そして.opendbファイルをコミット対象外に設定 ただし、プログラムはC4715が発生している。

--- ConsoleApplication5/Source.cpp
+++ ConsoleApplication5/Source.cpp
@@ -1,4 +1,3 @@
-#include <Windows.h>
 #include <iostream>
 #include <string>
 #include <boost/property_tree/ptree.hpp>
@@ -8,26 +7,17 @@
 #include <stdexcept>
 #include <type_traits>
 
-std::string WStringToString(std::wstring oWString) {
-	int iBufferSize = WideCharToMultiByte(CP_OEMCP, 0, oWString.c_str(), -1, (char *)NULL, 0, NULL, NULL);
-	char* cpMultiByte = new char[iBufferSize];
-	WideCharToMultiByte(CP_OEMCP, 0, oWString.c_str(), -1, cpMultiByte, iBufferSize, NULL, NULL);
-	std::string oRet(cpMultiByte, cpMultiByte + iBufferSize - 1);
-	delete[] cpMultiByte;
-	return oRet;
-}
-
 std::string data(boost::property_tree::ptree& pt, const char* directory) {
 	return pt.get_optional<std::string>(directory).get();
 }
 
-static std::string get_text_from_json(boost::property_tree::ptree& p, std::string tag) {
+static std::wstring get_text_from_json(boost::property_tree::ptree& p, std::string tag) {
 	auto target = p.get_optional<std::string>(tag);
 	if (!target) throw std::runtime_error("json reading failed");
 	static_assert(sizeof(wchar_t) == 2, "In function 'read json data' : Please check usage of 'std::codecvt_utf8_utf16'");
 	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>, wchar_t> convert;
 	auto str = convert.from_bytes(target.get());
-	return WStringToString(str);
+	return str;
 }
 
 static int get_int_from_json(boost::property_tree::ptree& p, std::string tag) {
@@ -42,8 +32,9 @@ int main(void) {
 		read_json("test.json", pt);
 		auto& p = pt.get_child("Data");
 		std::cout << data(p, "str") << std::endl << std::endl;
+		std::wcout.imbue(std::locale(""));
 		for (auto& child : p.get_child("info")) {
-			std::cout
+			std::wcout
 				<< "id: " << get_int_from_json(child.second, "id") << std::endl
 				<< "name:" << get_text_from_json(child.second, "name") << std::endl << std::endl;
 		}
